540656b39a25c2327064d29d4efd0944769acd28
Giorgos Dimtsas <gedim21@gmail.com>
2015-04-07 02:09:02 +0300

    No message.

--- embedding_lua/gen.bat
+++ embedding_lua/gen.bat
@@ -1,2 +1,3 @@
 @echo off
 swig -c++ -lua -o EngineLua.cpp Engine.i
+swig -lua -external-runtime swig.h
--- embedding_lua/main.cpp
+++ embedding_lua/main.cpp
@@ -4,6 +4,7 @@
 #include "Engine.h"
 #include "TransformationComponent.h"
 #include "Entity.h"
+#include "swig.h"
 
 extern "C"
 {
@@ -12,14 +13,38 @@ extern "C"
 #include "lualib.h"
 }
 
+extern "C"
+{
+  int luaopen_Engine(lua_State* L);
+}
+
 int main(int argc, char *argv[])
 {
-	Engine::Engine engine;
+  Engine::Engine engine;
   Engine::Entity entity(1);
 
   Engine::TransformationComponent tc;
   entity.addComponent(&tc);
 
   engine.addEntity(&entity);
+
+  //
+
+  lua_State* state = lua_open();
+  luaL_openlibs(state);
+  luaopen_Engine(state);
+
+  SWIG_Lua_NewPointerObj(state,
+                         &engine,
+                         SWIG_TypeQuery(state, "Engine::Engine *"),
+                         0);
+  lua_setglobal(state, "engine");
+
+  int status = luaL_dofile(state, "Example.lua");
+  if(status != 0)
+    {
+      std::cerr << lua_tostring(state, -1) << std::endl;
+      lua_pop(state, 1);
+    }
   return 0;
 }
