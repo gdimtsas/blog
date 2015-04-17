f7620d23e9f1623c26df4807a64b5c964c72d6b1
Giorgos Dimtsas <gedim21@gmail.com>
2015-04-17 18:36:22 +0300

    No message.

---
+++ basic_lua_embedding/calculator.lua
@@ -0,0 +1,15 @@
+function add(x, y)
+   return x + y
+end
+
+function sub(x, y)
+   return x - y
+end
+
+function mul(x, y)
+   return x * y
+end
+
+function div(x, y)
+   return x / y
+end
--- basic_lua_embedding/main.cpp
+++ basic_lua_embedding/main.cpp
@@ -10,22 +10,20 @@ extern "C"
 int basic(int argc, char *argv[])
 {
   // initialize lua
-  lua_State* L = lua_open();
+  lua_State* L = luaL_newstate();
 
-  // load all lua basic libs, like math, io, string etc...
+  // load all lua basic libs (like math, io, string etc...)
   luaL_openlibs(L);
 
-  int status = 0;
-
   // execute a lua statement from a string
-  status = luaL_dostring(L, "print('Hello from Lua!')");
+  int status = luaL_dostring(L, "print('Hello from Lua!')");
 
-  // or from a file
-  status = luaL_dofile(L, "example.lua");
+  // check for errors
   if(status != 0)
     {
       std::cerr << lua_tostring(L, -1) << std::endl;
       lua_pop(L, 1);
+      return -1;
     }
 
   // cleanup lua
@@ -34,51 +32,31 @@ int basic(int argc, char *argv[])
   return 0;
 }
 
-int functionCall(int argc, char *argv[])
+int luaFunctionCall(int argc, char *argv[])
 {
   // initialize lua
   lua_State* L = lua_open();
-
-  // load all lua basic libs, like math, io, string etc...
   luaL_openlibs(L);
 
-  // load the example.lua file
-  int status = luaL_dofile(L, "example.lua");
-  if(status != 0)
-    {
-      std::cerr << lua_tostring(L, -1) << std::endl;
-      lua_pop(L, 1);
-      return -1;
-    }
+  // load the calculator.lua file
+  luaL_dofile(L, "calculator.lua");
 
+  // push onto the stack the value of the global "add"
   lua_getglobal(L, "add");
-  if(!lua_isfunction(L, -1))
-    {
-      lua_pop(L, 1);
-      return -1;
-    }
 
   // push arguments to the stack
   lua_pushnumber(L, 7);
   lua_pushnumber(L, 11);
 
   // do the call (2 arguments, 1 result)
-  status = lua_pcall(L, 2, 1, 0);
-  if(status != 0)
-    {
-      std::cerr << lua_tostring(L, -1) << std::endl;
-      lua_pop(L, 1);
-      return -1;
-    }
+  lua_call(L, 2, 1);
 
-  // retrieve result
-  if (!lua_isnumber(L, -1))
+  // check the result (which is on the top of the stack)
+  if (lua_isnumber(L, -1))
     {
-      return -1;
+      lua_Number result = lua_tonumber(L, -1);
+      std::cout << "Result: " << result << std::endl;
     }
-
-  int result = lua_tonumber(L, -1);
-  std::cout << "Result: " << result << std::endl;
 
   // pop the result from the stack
   lua_pop(L, 1);
@@ -89,8 +67,46 @@ int functionCall(int argc, char *argv[])
   return 0;
 }
 
+extern "C"
+{
+  int doubleAndTriple(lua_State* L)
+  {
+    // retrieve the first (and only) argument
+    lua_Number n = luaL_checknumber(L, 1);
+
+    // push the first return value
+    lua_pushnumber(L, n * 2);
+    // and then the second
+    lua_pushnumber(L, n * 3);
+
+    // return the number of returned results
+    return 2;
+  }
+}
+
+int cFunctionCall(int argc, char *argv[])
+{
+  // initialize lua
+  lua_State* L = lua_open();
+  luaL_openlibs(L);
+
+  // register the doubleAndTriple function under the name "f"
+  lua_register(L, "f", doubleAndTriple);
+
+  // call the c function from lua
+  luaL_dostring(L, "a, b = f(5);");
+  luaL_dostring(L, "print('a = ' .. a)");
+  luaL_dostring(L, "print('b = ' .. b)");
+
+  // cleanup lua
+  lua_close(L);
+
+  return 0;
+}
+
 int main(int argc, char *argv[])
 {
-  basic(argc, argv);
-  functionCall(argc, argv);
+  //basic(argc, argv);
+  luaFunctionCall(argc, argv);
+  //cFunctionCall(argc, argv);
 }
