b43c318e4814d6a8ccf343f8edb24a905301a7ba
Giorgos Dimtsas <gedim21@gmail.com>
2015-04-26 19:26:40 +0300

    No message.

---
+++ mono_generics/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace MonoGenerics
+{
+  public class TestClass
+  {
+    public void GenericMethod<T>(T t)
+    {
+      Console.WriteLine(t);
+    }
+
+    public static IntPtr MakeGenericMethod(MethodInfo method, Type type)
+    {
+      return method.MakeGenericMethod(type).MethodHandle.Value;
+    }
+  }
+}
