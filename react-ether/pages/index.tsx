bc9acec0b35c0a242bbb5a26c6e484c78a10ca0e
Giorgos Dimtsas <gedim21@gmail.com>
2022-06-06 18:55:20 +0300

    Temp commit

--- react-ether/pages/index.tsx
+++ react-ether/pages/index.tsx
@@ -1,45 +1,66 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import NextLink from "next/link";
-import { VStack, Heading, Box, LinkOverlay, LinkBox } from "@chakra-ui/layout";
-import { Text, Button } from "@chakra-ui/react";
+import { VStack, HStack, Heading, Box } from "@chakra-ui/layout";
+import { Button, Flex, Spacer, Divider, Stat, StatLabel, StatNumber, StatHelpText, Badge } from "@chakra-ui/react";
 
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
+import { Web3Provider } from "@ethersproject/providers";
 
-declare let window: any;
+declare const window: any;
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState<string | undefined>();
   const [currentAccount, setCurrentAccount] = useState<string | undefined>();
   const [chainId, setChainId] = useState<number | undefined>();
-  const [chainname, setChainName] = useState<string | undefined>();
+  const [chainName, setChainName] = useState<string | undefined>();
 
   useEffect(() => {
     if (!currentAccount || !ethers.utils.isAddress(currentAccount)) return;
-    //client side code
-    if (!window.ethereum) return;
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    provider.getBalance(currentAccount).then((result) => {
-      setBalance(ethers.utils.formatEther(result));
-    });
-    provider.getNetwork().then((result) => {
-      setChainId(result.chainId);
-      setChainName(result.name);
-    });
-  }, [currentAccount]);
 
-  const onClickConnect = () => {
-    //client side code
     if (!window.ethereum) {
       console.log("please install MetaMask");
       return;
     }
 
-    //we can do it using ethers.js
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-    // MetaMask requires requesting permission to connect users accounts
+    fetchChainInfo(provider);
+    fetchBalance(provider);
+
+    // remove all listeners when the component is unmounted
+    return () => {
+      provider.removeAllListeners();
+    };
+  }, [currentAccount, chainId]);
+
+  function fetchBalance(provider: Web3Provider) {
+    if (!currentAccount) return;
+
+    provider.getBalance(currentAccount).then((balance) => {
+      setBalance(ethers.utils.formatEther(balance));
+    });
+  }
+
+  function fetchChainInfo(provider: Web3Provider) {
+    provider.getNetwork().then((network) => {
+      setChainId(network.chainId);
+      setChainName(network.name);
+    });
+  }
+
+  const connect = () => {
+    if (!window.ethereum) {
+      console.log("please install MetaMask");
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    window.ethereum.on("accountsChanged", onAccountChanged);
+
+    window.ethereum.on("chainChanged", onChainChanged);
+
     provider
       .send("eth_requestAccounts", [])
       .then((accounts) => {
@@ -48,10 +69,21 @@ const Home: NextPage = () => {
       .catch((e) => console.log(e));
   };
 
-  const onClickDisconnect = () => {
-    console.log("onClickDisConnect");
+  const disconnect = () => {
     setBalance(undefined);
     setCurrentAccount(undefined);
+  };
+
+  const onAccountChanged = (accounts: Array<string>) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setCurrentAccount(accounts[0]);
+    fetchBalance(provider);
+  };
+
+  const onChainChanged = () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    fetchChainInfo(provider);
+    fetchBalance(provider);
   };
 
   return (
@@ -59,54 +91,48 @@ const Home: NextPage = () => {
       <Head>
         <title>My DAPP</title>
       </Head>
-
-      <Heading as="h3" my={4}>
-        Explore Web3
-      </Heading>
-      <VStack>
-        <Box w="100%" my={4}>
-          {currentAccount ? (
-            <Button type="button" w="100%" onClick={onClickDisconnect}>
-              Account:{currentAccount}
-            </Button>
-          ) : (
-            <Button type="button" w="100%" onClick={onClickConnect}>
-              Connect MetaMask
-            </Button>
-          )}
-        </Box>
-        {currentAccount ? (
-          <Box mb={0} p={4} w="100%" borderWidth="1px" borderRadius="lg">
-            <Heading my={4} fontSize="xl">
-              Account info
-            </Heading>
-            <Text>ETH Balance of current account: {balance}</Text>
-            <Text>
-              Chain Info: ChainId {chainId} name {chainname}
-            </Text>
+      <Flex>
+        <Spacer />
+        <VStack w="35em">
+          <Spacer />
+          <Box w="100%">
+            {currentAccount ? (
+              <Button colorScheme="green" type="button" w="100%" onClick={disconnect}>
+                {currentAccount}
+              </Button>
+            ) : (
+              <Button colorScheme="red" type="button" w="100%" onClick={connect}>
+                Connect MetaMask
+              </Button>
+            )}
           </Box>
-        ) : (
-          <></>
-        )}
-
-        <Box my={4} p={4} w="100%" borderWidth="1px" borderRadius="lg">
-          <Heading my={4} fontSize="xl">
-            Task 2
-          </Heading>
-          <Text>DAPP with React/NextJS/Chakra</Text>
-        </Box>
-
-        <LinkBox my={4} p={4} w="100%" borderWidth="1px" borderRadius="lg">
-          <NextLink href="https://github.com/NoahZinsmeister/web3-react/tree/v6" passHref>
-            <LinkOverlay>
+          {currentAccount ? (
+            <Box mb={0} p={4} w="100%" borderWidth="1px" borderRadius="lg">
               <Heading my={4} fontSize="xl">
-                Task 3 with link
+                Account info
               </Heading>
-              <Text>Read docs of Web3-React V6</Text>
-            </LinkOverlay>
-          </NextLink>
-        </LinkBox>
-      </VStack>
+              <Divider />
+              <Stat>
+                <StatLabel>Balance</StatLabel>
+                <StatNumber>Ξ {balance}</StatNumber>
+                <StatHelpText>
+                  <HStack spacing="10px">
+                    <Badge colorScheme="green" variant="outline">
+                      {chainId}
+                    </Badge>
+                    <Badge colorScheme="blue" variant="outline">
+                      {chainName}
+                    </Badge>
+                  </HStack>
+                </StatHelpText>
+              </Stat>
+            </Box>
+          ) : (
+            <></>
+          )}
+        </VStack>
+        <Spacer />
+      </Flex>
     </>
   );
 };
